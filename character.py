b519304a993e7301bdc9b9a96989ce3ad9c0c7c3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-08 11:39:17 +0000

    add comments to make understanding easier

--- character.py
+++ character.py
@@ -1,21 +1,24 @@
 #Lista de criaturas
 criaturas = []
 
-#
+# input para cada monstro até termos 3
 for i in range(3):
     nome = input("Nome: ")
     vida = int(input("Vida: "))
     nível = int(input("Nível: "))
         
+    #organiza os monstros
     criaturas.append([nome, (vida, nível)])
 
+#Monstra os montros na consola
 print(criaturas)
 
+#organiza por nível
 for i in range(len(criaturas)):
     for j in range(i + 1, len(criaturas)):
         if criaturas[i][1][1] < criaturas[j][1][1]:
             criaturas[i], criaturas[j] = criaturas[j], criaturas[i]  
 
-
+#por cada organização, faz print descente
 for criatura in criaturas:
     print(criatura[0])
