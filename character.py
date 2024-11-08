3b9645c5f9970e3c3c9328edf2c7fc409667a0d2
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-11-08 11:34:47 +0000

    add int values to vida e nível and added sorter

--- character.py
+++ character.py
@@ -2,10 +2,16 @@ criaturas = []
 
 for i in range(3):
     nome = input("Nome: ")
-    vida = input("Vida: ")
-    nível = input("Nível: ")
-    
-criaturas.append([nome, (vida, nível)])
+    vida = int(input("Vida: "))
+    nível = int(input("Nível: "))
+        
+    criaturas.append([nome, (vida, nível)])
 
 print(criaturas)
 
+
+criaturas.sort(key=lambda x: x[1][1], reverse=True)
+
+
+for criatura in criaturas:
+    print(criatura[0])
